1b371e7b0fb481c051e69e4b485c00eadd04f5ef
irow0433 <31513820+irow0433@users.noreply.github.com>
2017-09-07 09:57:20 -0600

    Added scanner

---
+++ src/onmyownpackage/Samplecontroller.java
@@ -0,0 +1,11 @@
+package onmyownpackage;
+
+public class Samplecontroller 
+{
+	
+		public void start()
+	{
+		System.out.println("I did it on my own");
+		System.out.println("We did it!!");
+	}
+}
---
+++ src/onmyownpackage/Samplerunner.java
@@ -0,0 +1,10 @@
+package onmyownpackage;
+
+public class Samplerunner 
+{
+	public static void main (String [] args)
+	{
+		Samplecontroller myApp = new Samplecontroller();
+		myApp.start();
+	}
+}
